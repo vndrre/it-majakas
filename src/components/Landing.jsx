196e3742fbff1d14c72178dfec1f2608ba2540d6
AndreLp5 <aleppik7@gmail.com>
2024-06-04 10:54:40 +0300

    a

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -6,8 +6,8 @@ const Landing = () => {
     <div className='bg-[#F7F6F6] md:p-20 font-manrope'>
         <div className='grid grid-cols-1 gap-10 lg:my-36'>
             <div className='grid gap-5 lg:px-0 px-10'>
-                <h1 className="md:text-[64px] text-[32px] font-medium">Sinu tee tulevikutehnoloogiatesse!</h1>
-                <p className='md:text-[20px] text-[#4D4D4D] leading-9'>Alusta oma karjääri IT, raamatupidamise või väikeettevõtluse valdkonnas. 
+                <h1 className="lg:text-[64px] text-[34px] font-medium">Sinu tee tulevikutehnoloogiatesse!</h1>
+                <p className='lg:text-[20px] text-[#4D4D4D] leading-9'>Alusta oma karjääri IT, raamatupidamise või väikeettevõtluse valdkonnas. 
                     Leia parimad <br/> õppimisvõimalused ning võta esimene samm oma unistuste tuleviku suunas meie juures.
                 </p>
             </div>
